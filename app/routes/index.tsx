5df5d97ac51de3613252b62fdb3729e3dba3f398
Trent <t2kocurek@gmail.com>
2022-08-31 19:37:21 -0500

    mvp2

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -35,22 +35,31 @@ export default function Index() {
   }, [link]);
 
   return (
-    <main className="max-w-7xl mx-auto">
+    <main className="max-w-7xl mx-auto mt-20">
       <link.Form method="post" ref={form} className="flex flex-col gap-y-3">
         <div>
           <label htmlFor="email" className="block text-sm font-medium text-gray-700">Shortcode</label>
-          <div className="mt-1">
-            <input type="text" name="shortcode" id="shortcode" className="py-4 px-3 block w-full rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-red-500 sm:text-sm" placeholder="you@example.com" />
+          <div className="relative mt-1 rounded-md shadow-sm">
+            <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+              <span className="text-gray-500 sm:text-sm tracking-wide">https://eqp.li/nk/</span>
+            </div>
+            <input
+              type="text"
+              name="shortcode"
+              id="shortcode"
+              className="py-4 px-3 pl-32 block w-full rounded-md border border-gray-300 shadow-sm focus:border-gray-500 focus:ring-red-500 sm:text-sm"
+              placeholder="code"
+            />
           </div>
         </div>
         <div>
           <label htmlFor="email" className="block text-sm font-medium text-gray-700">URL</label>
           <div className="mt-1">
-            <input type="text" name="longUrl" id="longUrl" className="py-4 px-3 block w-full rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-red-500 sm:text-sm" placeholder="you@example.com" />
+            <input type="text" name="longUrl" id="longUrl" className="py-4 px-3 block w-full rounded-md border border-gray-300 shadow-sm focus:border-gray-500 focus:ring-red-500 sm:text-sm" placeholder="google.com" />
           </div>
         </div>
         <div>
-          <button type="submit" className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-3 py-2 text-sm font-medium leading-4 text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Create Link</button>
+          <button type="submit" className="inline-flex items-center rounded-md border border-transparent bg-gray-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2">Create Link</button>
         </div>
       </link.Form>
 
