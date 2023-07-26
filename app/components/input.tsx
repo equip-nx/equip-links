845972bed6426936c6ffd3bbeee09b2a653649bd
Trent Kocurek <t2kocurek@gmail.com>
2023-07-26 07:30:02 -0500

    Fix input label component

--- app/components/input.tsx
+++ app/components/input.tsx
@@ -7,7 +7,7 @@ export default function Input({ type, name, placeholder, label, className }: {
 }) {
   return (
     <div className="bg-white rounded-md border border-gray-300 px-3 py-2 shadow-sm focus-within:border-gray-600 focus-within:ring-0 focus-within:ring-gray-600">
-      <label htmlFor="name" className="block text-xs font-medium text-gray-900">
+      <label htmlFor={name} className="block text-xs font-medium text-gray-900">
         {label}
       </label>
       <input
