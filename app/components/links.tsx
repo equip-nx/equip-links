1502b15e3167281b5c750bc67ae13bdabc6bbe3a
Trent Kocurek <t2kocurek@gmail.com>
2023-07-27 22:19:40 -0500

    Update link actions

--- app/components/links.tsx
+++ app/components/links.tsx
@@ -54,7 +54,13 @@ export default function Links({ links }: { links: Link[] }) {
                     scope="col"
                     className="px-3 py-3 text-center text-xs font-medium uppercase tracking-wide text-gray-500"
                   >
-                    Clicks
+                    Actions
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-3 py-3 text-center text-xs font-medium uppercase tracking-wide text-gray-500"
+                  >
+                    Visits
                   </th>
                 </tr>
               </thead>
@@ -65,25 +71,25 @@ export default function Links({ links }: { links: Link[] }) {
                       {link.shortcode}
                     </td>
                     <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{link.longUrl}</td>
-                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500 flex items-center gap-x-3">
-                      <span>
-                        {link.shortUrl}
-                      </span>
-                      <span className="cursor-pointer text-gray-500" onClick={() => copyToClipboard(link.shortUrl)}>
+                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                      {link.shortUrl}
+                    </td>
+                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                      {new Date(link.createdAt).toLocaleDateString('en-us', { weekday: "long", year: "numeric", month: "short", day: "numeric" })}
+                    </td>
+                    <td className="flex items-center gap-x-4 px-3 py-4 justify-center">
+                      <span title="Copy Link" className="cursor-pointer text-gray-500" onClick={() => copyToClipboard(link.shortUrl)}>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
                           <path strokeLinecap="round" strokeLinejoin="round" d="M15.666 3.888A2.25 2.25 0 0013.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 01-.75.75H9a.75.75 0 01-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 011.927-.184" />
                         </svg>
                       </span>
-                      <span className="cursor-pointer text-gray-500" onClick={() => downloadQRCode(link.shortcode)}>
-                        <QRCodeCanvas id={link.shortcode} value={link.shortUrl} fgColor="#1e344a" bgColor="#ffffff" className="hidden" />
+                      <span title="Download QR Code" className="cursor-pointer text-gray-500" onClick={() => downloadQRCode(link.shortcode)}>
+                        <QRCodeCanvas id={link.shortcode} value={link.shortUrl} fgColor="#ffffff" bgColor="#000000" className="hidden" />
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
                           <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 4.875c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5A1.125 1.125 0 013.75 9.375v-4.5zM3.75 14.625c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5a1.125 1.125 0 01-1.125-1.125v-4.5zM13.5 4.875c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5A1.125 1.125 0 0113.5 9.375v-4.5z" />
                           <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 6.75h.75v.75h-.75v-.75zM6.75 16.5h.75v.75h-.75v-.75zM16.5 6.75h.75v.75h-.75v-.75zM13.5 13.5h.75v.75h-.75v-.75zM13.5 19.5h.75v.75h-.75v-.75zM19.5 13.5h.75v.75h-.75v-.75zM19.5 19.5h.75v.75h-.75v-.75zM16.5 16.5h.75v.75h-.75v-.75z" />
                         </svg>
                       </span>
-                    </td>
-                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                      {new Date(link.createdAt).toLocaleDateString('en-us', { weekday: "long", year: "numeric", month: "short", day: "numeric" })}
                     </td>
                     <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500 text-center">{link.clicks}</td>
                   </tr>
